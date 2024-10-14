1416bea39506fffc965fa6ccac498d8aeb8f8136
gmh5225 <2315157@qq.com>
2024-10-14 20:00:01 +0800

    update

---
+++ script/Deploy.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "../src/TokenBank.sol";
+import "../src/SimpleToken.sol";
+
+contract DeployTokenBankAndTokenScript is Script {
+    function setUp() public { }
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("SEPOLIA_WALLET_PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy SimpleToken
+        SimpleToken token = new SimpleToken(1_000_000 * 10 ** 18); // 1,000,000 tokens
+        console2.log("SimpleToken deployed to:", address(token));
+
+        // deploy TokenBank
+        TokenBank bank = new TokenBank(address(token));
+        console2.log("TokenBank deployed to:", address(bank));
+
+        console2.log("Deployed by:", deployerAddress);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ script/DepositToTokenBank.s.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "../src/TokenBank.sol";
+import "../src/SimpleToken.sol";
+
+contract DepositToTokenBankScript is Script {
+    function setUp() public { }
+
+    function run() public {
+        uint256 userPrivateKey = vm.envUint("RECIPIENT_ADDRESS");
+        address userAddress = vm.addr(userPrivateKey);
+        address tokenAddress = vm.envAddress("TOKEN_CONTRACT_ADDRESS");
+        address bankAddress = vm.envAddress("BANK_CONTRACT_ADDRESS");
+
+        vm.startBroadcast(userPrivateKey);
+
+        SimpleToken token = SimpleToken(tokenAddress);
+        TokenBank bank = TokenBank(bankAddress);
+
+        uint256 amountToDeposit = 100 * 10 ** 18; // deposit 100 tokens
+
+        // approve TokenBank to use tokens
+        token.approve(bankAddress, amountToDeposit);
+        console2.log("Approved TokenBank to use tokens");
+
+        // deposit tokens to TokenBank
+        bank.deposit(amountToDeposit);
+        console2.log("Deposited tokens to TokenBank");
+
+        // check balance after deposit
+        uint256 balance = bank.getDepositAmount(userAddress);
+        console2.log("User balance in TokenBank:", balance);
+
+        vm.stopBroadcast();
+    }
+}
